701166735c34017b42d54b6bb1d3eb566569fa93
Reiya YOSHIHARA <k21rs141@st.kyusan-u.ac.jp>
2023-07-12 16:51:28 +0900

    (仮)完成

--- Calender.java
+++ Calender.java
@@ -4,33 +4,60 @@ import java.util.Collections;
 
 
 class Reserve{
+	int year;
 	int month;
 	int date;//日にち
 	int rental;//貸し出し時間
-	String name;//予約者の名前
+	String title;//	目的
 	
-	void set(int month, int date, int rental, String name) {
+	void set(int year, int month, int date, int rental, String title) {
+		this.year = year;
 		this.month = month;
 		this.date = date;
 		this.rental = rental;
-		this.name = name;
+		this.title = title;
 	}
 }
 
 public class Calender {
 
 	public static void main(String[] args) {
+		//------------------------予約のリスト入力---------------------
+				Reserve r1 = new Reserve();
+				Reserve r2 = new Reserve();
+				Reserve r3 = new Reserve();
+				Reserve r4 = new Reserve();
+				
+				r1.set(2023, 8, 2, 3, "試合");
+				r2.set(2023, 7, 5, 1, "試合");
+				r3.set(2023, 7, 26, 2, "会議");
+				r4.set(2023, 7, 5, 3, "打ち合わせ");
+				
+				System.out.println("予約済み一覧");
+				showReserve(r1);
+				showReserve(r2);
+				showReserve(r3);
+				showReserve(r4);
+				
+				Reserve [] order = new Reserve[4];
+				order [0] = r1;
+				order [1] = r2;
+				order [2] = r3;
+				order [3] = r4;
+			
+		
 		Calendar cl = Calendar.getInstance();
 		//-----------------今月-----------------------------
+		System.out.println("\n");
 		int thisYear = cl.get(Calendar.YEAR); //今年
 		int thisMonth = cl.get(Calendar.MONTH); //今月(1月=0、2月=1であるため配列monthの添字に使用)
-		outputCalendar(thisYear,thisMonth);
+		outputCalendar(thisYear,thisMonth, order);
 		
 		//-----------------来月-----------------------------
 		System.out.println("\n");
 		int nextYear = cl.get(Calendar.YEAR); //今年
 		int nextMonth = cl.get(Calendar.MONTH) + 1; //来月
-		outputCalendar(nextYear,nextMonth);
+		outputCalendar(nextYear,nextMonth, order);
 		
 		/*
 		//-----------------------任意の年、月の確認用-----------------------
@@ -40,38 +67,25 @@ public class Calender {
 		outputCalendar(otherYear,otherMonth - 1);
 		*/
 		
-		//------------------------予約のリスト入力---------------------
-		Reserve r1 = new Reserve();
-		Reserve r2 = new Reserve();
-		Reserve r3 = new Reserve();
-		Reserve r4 = new Reserve();
-		
-		r1.set(6, 1, 3, "吉田");
-		r2.set(6, 5, 1, "中原");
-		r3.set(6, 26, 2, "久木原");
-		r4.set(6, 5, 3, "官");
-		
-		System.out.println("予約済み一覧");
-		showReserve(r1);
-		showReserve(r2);
-		showReserve(r3);
-		showReserve(r4);
 		
 	}
+	
 	//--------------------------------予約リスト表示----------------------------
 	static void showReserve(Reserve r) {
+		
 		if(r.rental == 1) {
-			System.out.println(r.month + "月" + r.date + "日→[" + r.rental + "] 9:00 - 11:50 " + r.name + "様");
+			System.out.println(r.year + "年" + r.month + "月" + r.date + "日→[" + r.rental + "] 9:00 - 11:50 " + r.title);
 		}
 		else if(r.rental ==2) {
-			System.out.println(r.month + "月" + r.date + "日→[" + r.rental + "] 11:00 - 14:50 " + r.name + "様");
+			System.out.println(r.year + "年" + r.month + "月" + r.date + "日→[" + r.rental + "] 11:00 - 14:50 " + r.title);
 		} 
 		else {
-			System.out.println(r.month + "月" + r.date + "日→[" + r.rental + "] 15:00 - 17:50 " + r.name + "様");
+			System.out.println(r.year + "年" + r.month + "月" + r.date + "日→[" + r.rental + "] 15:00 - 17:50 " + r.title);
 		}
 	}
 	
-	public static void outputCalendar(int inputYear, int inputMonth) {
+	
+	public static void outputCalendar(int inputYear, int inputMonth, Reserve [] order) {
 		Calendar cl = Calendar.getInstance();
 		
 		String[] month = {"January","February","March","April","May","June",
@@ -108,6 +122,7 @@ public class Calender {
 			}			
 		}
 		System.out.println("\n");
+		
 		//--------------------------営業時間表示--------------------------
 		System.out.println("営業時間  " + starttime +" - " + endtime + "\n");
 		
@@ -118,17 +133,41 @@ public class Calender {
 		}
 		System.out.println();
 		
+		
 		//--------------------日付(曜日)と空き状況の表示「縦に表示」--------------------------
 		System.out.println("空き状況");
 		for (int i = firstDay; i <= lastDay; i++) {
 			int u =(firstDayWeek  + i -1) % 7;
+			
+			String name1 ="○";
+			String name2 ="○";
+			String name3 ="○";
+			
+			/*
+			String [] name = new String[3];
+			for(int a = 0; a < 3; a++) {
+				 name[a] = "○";
+			}
+			*/
+			for (int x = 0; x < order.length; x++) {
+				if(order[x].year == inputYear && order[x].month -1 == inputMonth && order[x].date == i) {
+					if(order[x].rental == 1) {							
+						name1 = order[x].title;
+					}if(order[x].rental == 2) {							
+						name2 = order[x].title;
+					}if(order[x].rental == 3) {							
+						name3 = order[x].title;
+					} 
+				}
+			}
+			
 			if(u ==0) {//１週間の区切りの線
 				System.out.println("-------------------------------------");
 			}
 			if (u == 2) {//定休日の表示
 				System.out.println(i + "(" + week[u] + ")→定休日");
 			} else {
-				System.out.println(i + "(" + week[u] +")→[1]:" + " [2]:" + " [3]:" );
+				System.out.println(i + "(" + week[u] +")→[1]:" + name3 + " [2]:" +name2 + " [3]:" +name3);
 			}
 			
 		}
